78c7ac9b5d43139077ab44ba9e5b972a06dfa8d6
digislexia <digislexia@gmail.com>
2018-06-18 11:38:29 +0300

    wip

---
+++ src/components/Another.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      Another component
+      <ul>
+        <li>
+          <router-link :to="{name:'HelloWorld'}">hello world</router-link>
+        </li>
+        <li>
+          <router-link :to="{name:'Another'}">another component</router-link>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Another"
+    }
+</script>
+
+<style scoped>
+
+</style>
